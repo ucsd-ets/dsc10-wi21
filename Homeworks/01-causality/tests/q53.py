8d81aaa33992588aec55ee89225b1818209618db
Yang Li <yal319@ucsd.edu>
2021-01-09 18:54:25 -0800

    add HW1

---
+++ Homeworks/01-causality/tests/q53.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q53',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> disposable_trait in {1, 2, 3}\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
---
+++ Homeworks/01-causality/tests/q81.py
@@ -0,0 +1,1 @@
+test = {'name': 'q81', 'points': 1, 'suites': [{'cases': [{'code': '>>> no_rct in {1, 2, 3, 4}\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
