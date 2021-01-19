e92ebca77b0a655fa3da57c77565cb01291e5422
Yang Li <yal319@ucsd.edu>
2021-01-19 09:32:23 -0800

    Fix q12 test case for lab03

--- labs/03-apply_vis/tests/q12.py
+++ labs/03-apply_vis/tests/q12.py
@@ -1,3 +1,3 @@
 test = {   'name': 'q12',
     'points': 1,
-    'suites': [{'cases': [{'code': ">>> mark_hurd_pay_string = '$53.25 '\n", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [{'cases': [{'code': ">>> mark_hurd_pay_string == '$53.25 '\nTrue", 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
