13938461d42b634783b4d64e2063a6ad69a200b2
Yang Li <yal319@ucsd.edu>
2021-01-26 13:33:05 -0800

    Update hw3

--- Homeworks/03-functions_visualizations/tests/q34.py
+++
@@ -1,1 +0,0 @@
-test = {'name': 'q34', 'points': 1, 'suites': [{'cases': [{'code': '>>> spike in {1,2,3,4}\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
--- Homeworks/03-functions_visualizations/tests/q3_3.py
+++ Homeworks/03-functions_visualizations/tests/q3_3.py
@@ -1,3 +1,3 @@
 test = {   'name': 'q3_3',
     'points': 1,
-    'suites': [{'cases': [{'code': '>>> isinstance(trends, bpd.DataFrame)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
+    'suites': [{'cases': [{'code': '>>> isinstance(year_first_appeared, bpd.DataFrame)\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
