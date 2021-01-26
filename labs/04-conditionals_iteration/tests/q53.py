810996d7fa44c8ec0f0f3213c1b752dbe715e7f8
Yang Li <yal319@ucsd.edu>
2021-01-26 13:24:55 -0800

    Add Lab04

---
+++ labs/04-conditionals_iteration/tests/q53.py
@@ -0,0 +1,3 @@
+test = {   'name': 'q53',
+    'points': 1,
+    'suites': [{'cases': [{'code': '>>> theoretical_probability == 16/20\nTrue', 'hidden': False, 'locked': False}], 'scored': True, 'setup': '', 'teardown': '', 'type': 'doctest'}]}
