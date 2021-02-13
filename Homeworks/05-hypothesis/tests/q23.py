448eed8d6b17203cf6caaba5e40a14c26dc78d25
Yang Li <yal319@ucsd.edu>
2021-02-13 15:05:22 -0800

    Add HW5

---
+++ Homeworks/05-hypothesis/tests/q23.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q23',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> type(unreasonable_test_statistics) == int\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> 0 < unreasonable_test_statistics < 5\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
---
+++ Homeworks/05-hypothesis/tests/q24.py
@@ -0,0 +1,8 @@
+test = {   'name': 'q24',
+    'points': 1,
+    'suites': [   {   'cases': [   {'code': '>>> isinstance(tvd_color, np.ndarray)\nTrue', 'hidden': False, 'locked': False},
+                                   {'code': '>>> len(tvd_color) == 5000\nTrue', 'hidden': False, 'locked': False}],
+                      'scored': True,
+                      'setup': '',
+                      'teardown': '',
+                      'type': 'doctest'}]}
