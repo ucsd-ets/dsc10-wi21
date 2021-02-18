2677d6efaed8ad08fca71666fafad9cc42d05f52
Janine Tiefenbruck <jlobue@eng.ucsd.edu>
2021-02-18 05:35:55 -0800

    fix lab 6 question 1.3

--- labs/06-resampling_and_the_bootstrap/tests/q13.py
+++ labs/06-resampling_and_the_bootstrap/tests/q13.py
@@ -1,6 +1,6 @@
 test = {   'name': 'q13',
     'points': 1,
-    'suites': [   {   'cases': [{'code': '>>> len(on_time_scores)==40\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> len(late_scores)==123\nTrue', 'hidden': False, 'locked': False}],
+    'suites': [   {   'cases': [{'code': '>>> len(on_time_scores)==123\nTrue', 'hidden': False, 'locked': False}, {'code': '>>> len(late_scores)==40\nTrue', 'hidden': False, 'locked': False}],
                       'scored': True,
                       'setup': '',
                       'teardown': '',
